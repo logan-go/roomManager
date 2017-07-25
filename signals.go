9ee5d780cac829278264b31fbb1e96b45c9b61fd
logan <logan.sui@loganx.org>
2017-07-26 01:10:45 +0800

    添加日志记录

--- signals.go
+++ signals.go
@@ -11,6 +11,8 @@ func ProcessSignals() {
 	if DETAILED_LOG_FLAG {
 		sig := make(chan os.Signal, 1)
 		signal.Notify(sig, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGHUP)
+
+		_ = <-sig
 		fmt.Println("[共建立连接]：", LinkedCounter)
 		fmt.Println("[共断开连接]：", ClosedCounter)
 		fmt.Println("[共发送消息]：", SendCounter)
